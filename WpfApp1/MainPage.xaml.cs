9e034bbada81b6dab0db236813f76936200de0ce
Eureka <747417582@qq.com>
2024-06-25 15:13:42 +0800

    feat: Client断开连接

--- WpfApp1/MainPage.xaml.cs
+++ WpfApp1/MainPage.xaml.cs
@@ -1,32 +1,20 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.IO;
 using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.IO;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using WpfApp1;
-using static WpfApp1.AesEncryptionFunctions;
+using Newtonsoft.Json;
+using static Utils.AesEncryption;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// MainPage.xaml 的交互逻辑
-    /// </summary>
     public partial class MainPage : Page
     {
+        private TcpClient _client;
+        private NetworkStream _stream;
+        private CancellationTokenSource _cancellationTokenSource;
+
         public MainPage()
         {
             InitializeComponent();
@@ -58,10 +46,11 @@ namespace WpfApp1
         {
             NavigationService.Navigate(new RegisterPage());
         }
-        private void SaveConfig(Config config,string ConfigFilePath)
+        
+        private void SaveConfig(Config config, string configFilePath)
         {
             var json = JsonConvert.SerializeObject(config, Formatting.Indented);
-            File.WriteAllText(ConfigFilePath, json);
+            File.WriteAllText(configFilePath, json);
         }
 
         public void connect_Click(object sender, RoutedEventArgs e)
@@ -78,11 +67,11 @@ namespace WpfApp1
             config.Username = username.Text;
             SaveConfig(config, configPath);
 
-            // 在一个新的线程中启动 SOCKS5 客户端。
-            Task.Run(() => StartSocks5Client(serverAddress, serverPort, userName, password, localPort));
+            _cancellationTokenSource = new CancellationTokenSource();
+            Task.Run(() => StartSocks5Client(serverAddress, serverPort, userName, password, localPort, _cancellationTokenSource.Token));
         }
 
-        private void StartSocks5Client(string serverAddress, int serverPort, string userName, string password, int localPort)
+        private void StartSocks5Client(string serverAddress, int serverPort, string userName, string password, int localPort, CancellationToken cancellationToken)
         {
             byte[] aesKey;
             byte[] aesIV;
@@ -90,18 +79,18 @@ namespace WpfApp1
             {
                 Log("Starting SOCKS5 client...");
 
-                using (TcpClient client = new TcpClient(serverAddress, serverPort))
-                using (NetworkStream stream = client.GetStream())
+                using (_client = new TcpClient(serverAddress, serverPort))
+                using (_stream = _client.GetStream())
                 {
                     Log("Connected to server");
 
                     // 握手：socks版本5，支持2种认证：无认证和用户名密码
                     byte[] handshakeRequest = new byte[] { 0x05, 0x02, 0x00, 0x02 };
-                    stream.Write(handshakeRequest, 0, handshakeRequest.Length);
+                    _stream.Write(handshakeRequest, 0, handshakeRequest.Length);
 
                     //接收：socks版本5，要求用户名和密码
                     byte[] handshakeResponse = new byte[2];
-                    stream.Read(handshakeResponse, 0, handshakeResponse.Length);
+                    _stream.Read(handshakeResponse, 0, handshakeResponse.Length);
 
                     if (handshakeResponse[1] == 0x02)
                     {
@@ -111,19 +100,18 @@ namespace WpfApp1
                         byte[] userNameBytes = Encoding.ASCII.GetBytes(userName);
                         byte[] passwordBytes = Encoding.ASCII.GetBytes(password);
 
-
                         byte[] authRequest = new byte[3 + userNameBytes.Length + passwordBytes.Length];
-                        authRequest[0] = 0x01;//子协议版本1
-                        authRequest[1] = (byte)userNameBytes.Length;//用户名字节长度
+                        authRequest[0] = 0x01; //子协议版本1
+                        authRequest[1] = (byte)userNameBytes.Length; //用户名字节长度
 
-                        Array.Copy(userNameBytes, 0, authRequest, 2, userNameBytes.Length);//复制用户名
-                        authRequest[2 + userNameBytes.Length] = (byte)passwordBytes.Length;//密码字节长度
-                        Array.Copy(passwordBytes, 0, authRequest, 3 + userNameBytes.Length, passwordBytes.Length);//复制密码
+                        Array.Copy(userNameBytes, 0, authRequest, 2, userNameBytes.Length); //复制用户名
+                        authRequest[2 + userNameBytes.Length] = (byte)passwordBytes.Length; //密码字节长度
+                        Array.Copy(passwordBytes, 0, authRequest, 3 + userNameBytes.Length, passwordBytes.Length); //复制密码
 
-                        stream.Write(authRequest, 0, authRequest.Length);
+                        _stream.Write(authRequest, 0, authRequest.Length);
 
                         byte[] authResponse = new byte[4];
-                        stream.Read(authResponse, 0, authResponse.Length);
+                        _stream.Read(authResponse, 0, authResponse.Length);
 
                         if (authResponse[1] != 0x00)
                         {
@@ -131,7 +119,7 @@ namespace WpfApp1
                             MessageBox.Show("Authentication failed");
                             return;
                         }
-                        else if(authResponse[1] == 0x00)
+                        else if (authResponse[1] == 0x00)
                         {
                             Log("Authentication successfully");
                         }
@@ -145,7 +133,7 @@ namespace WpfApp1
                         int bytesRead = 0;
                         while (bytesRead < publicKeyLength)
                         {
-                            int result = stream.Read(publicKeyBytes, bytesRead, publicKeyLength - bytesRead);
+                            int result = _stream.Read(publicKeyBytes, bytesRead, publicKeyLength - bytesRead);
                             if (result == 0)
                             {
                                 throw new Exception("Connection closed unexpectedly.");
@@ -158,7 +146,6 @@ namespace WpfApp1
                         Log("Public key received successfully");
 
                         // 生成AES密钥
-                        
                         using (Aes aes = Aes.Create())
                         {
                             aes.GenerateKey();
@@ -169,21 +156,16 @@ namespace WpfApp1
 
                             // 加密AES密钥
                             byte[] encryptedAesKey = rsa.Encrypt(aesKey, RSAEncryptionPadding.OaepSHA256);
-                            // byte[] encryptedAesIV = rsa.Encrypt(aesIV, RSAEncryptionPadding.OaepSHA256);
-
-                            // Send encrypted AES key
                             byte[] keyLength = BitConverter.GetBytes((short)encryptedAesKey.Length);
-                            Array.Reverse(keyLength);  // Always send length in big endian
+                            Array.Reverse(keyLength); // Always send length in big endian
                             Log("keyLength[0]", keyLength[0]);
                             Log("keyLength[1]", keyLength[1]);
-                            stream.Write(keyLength, 0, keyLength.Length);
-                            stream.Write(encryptedAesKey, 0, encryptedAesKey.Length);
-                            stream.Write(aesIV, 0, aesIV.Length);
-                            // stream.Write(encryptedAesIV, 0, encryptedAesIV.Length);
+                            _stream.Write(keyLength, 0, keyLength.Length);
+                            _stream.Write(encryptedAesKey, 0, encryptedAesKey.Length);
+                            _stream.Write(aesIV, 0, aesIV.Length);
                         }
 
                         Log("AES key exchange successful");
-
                     }
                     else
                     {
@@ -191,9 +173,9 @@ namespace WpfApp1
                         MessageBox.Show("Server did not select username/password authentication");
                         return;
                     }
+
                     string destination_ip = "103.235.46.96";
                     ushort destination_port = 80;
-                    // Connect Request
                     byte[] destinationAddress = ParseIpAddress(destination_ip); // Use the TextBox for the destination IP
                     ushort destinationPort = destination_port; // Use the TextBox for the destination port
 
@@ -206,10 +188,10 @@ namespace WpfApp1
                     connectRequest[8] = (byte)(destinationPort >> 8);
                     connectRequest[9] = (byte)destinationPort;
 
-                    stream.Write(connectRequest, 0, connectRequest.Length);
+                    _stream.Write(connectRequest, 0, connectRequest.Length);
 
                     byte[] connectResponse = new byte[10];
-                    stream.Read(connectResponse, 0, connectResponse.Length);
+                    _stream.Read(connectResponse, 0, connectResponse.Length);
 
                     if (connectResponse[1] != 0x00)
                     {
@@ -219,91 +201,81 @@ namespace WpfApp1
                     }
 
                     Log("Connection request successful");
-                    // 通过代理服务器与目标服务器通信
+                    
                     string httpRequest = "GET / HTTP/1.1\r\nHost: baidu.com\r\nConnection: close\r\n\r\n";
                     byte[] httpRequestBytes = Encoding.ASCII.GetBytes(httpRequest);
-                    // 加密 HTTP 请求
                     byte[] encryptedHttpRequestBytes = EncryptWithAES(httpRequestBytes, aesKey, aesIV);
 
-                    // 发送加密后的请求
-                    stream.Write(encryptedHttpRequestBytes, 0, encryptedHttpRequestBytes.Length);
-                    Log("request send successfully");
-                    // stream.Write(httpRequestBytes, 0, httpRequestBytes.Length);
-                    // 读取响应并显示
+                    _stream.Write(encryptedHttpRequestBytes, 0, encryptedHttpRequestBytes.Length);
+                    Log("Request sent successfully");
+
                     byte[] buffer = new byte[8192];
                     using (MemoryStream memoryStream = new MemoryStream())
                     {
                         int bytesRead;
                         try
                         {
-                            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                            while ((bytesRead = _stream.Read(buffer, 0, buffer.Length)) > 0)
                             {
-                                // 写入到内存流
+                                if (cancellationToken.IsCancellationRequested)
+                                {
+                                    Log("Disconnection requested, stopping read operation.");
+                                    break;
+                                }
+
                                 memoryStream.Write(buffer, 0, bytesRead);
                                 Log($"Read {bytesRead} bytes from stream.");
 
-                                // 解密从流中读取的数据
                                 byte[] encryptedData = memoryStream.ToArray();
-                                byte[] decryptedData = DecryptWithAES(encryptedData, aesKey, aesIV);
+                                byte[] decryptedData = DecryptWithAES(encryptedData.Take(bytesRead).ToArray(), aesKey, aesIV);
 
-                                // 将解密后的数据转换为字符串并处理
                                 string decryptedString = Encoding.ASCII.GetString(decryptedData);
                                 Log("Decrypted Data: " + decryptedString);
+                                if (string.IsNullOrEmpty(decryptedString))
+                                {
+                                    Log("Decrypted response is empty.");
+                                }
+                                else
+                                {
+                                    File.WriteAllText("response.html", decryptedString);
+                                    Log("Response written to response.html");
+                                }
                             }
                         }
                         catch (IOException e)
                         {
-                            Log($"IOException: {e.Message}");
-                        }
-
-
-                        Log("target location");
-                        byte[] encryptedResponseBytes = memoryStream.ToArray();
-
-                        if (encryptedResponseBytes.Length == 0)
-                        {
-                            Log("No data was read from the stream.");
-                            return;
-                        }
-
-                        byte[] decryptedResponseBytes = DecryptWithAES(encryptedResponseBytes, aesKey, aesIV);
-
-                        if (decryptedResponseBytes.Length == 0)
-                        {
-                            Log("Decryption failed or produced no output.");
-                            return;
-                        }
-
-                        string responseString = Encoding.ASCII.GetString(decryptedResponseBytes);
-
-                        if (string.IsNullOrEmpty(responseString))
-                        {
-                            Log("Decrypted response is empty.");
-                        }
-                        else
-                        {
-                            File.WriteAllText("response.html", responseString);
-                            Log("Response written to response.html");
+                            if (!cancellationToken.IsCancellationRequested)
+                            {
+                                Log($"IOException: {e.Message}");
+                            }
+                            else
+                            {
+                                Log("Operation canceled.");
+                            }
                         }
                     }
-
-                    /*// Start TcpListener to listen on local port
-                    TcpListener listener = new TcpListener(IPAddress.Loopback, localPort);
-                    listener.Start();
-                    Log("Listening on port " + localPort);*/
-
-                    /*while (true)
-                    {
-                        TcpClient localClient = listener.AcceptTcpClient();
-                        Task.Run(() => HandleClient(localClient, client));
-                    }*/
                 }
             }
             catch (Exception ex)
             {
-                Log("Error: " + ex.Message);
-                MessageBox.Show("Error: " + ex.Message);
+                if (!cancellationToken.IsCancellationRequested)
+                {
+                    Log("Error: " + ex.Message);
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                else
+                {
+                    Log("Operation canceled.");
+                }
             }
+        }
+
+        public void disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            _cancellationTokenSource?.Cancel();
+            _stream?.Close();
+            _client?.Close();
+            Log("Disconnected from server.");
         }
 
         private byte[] ParseIpAddress(string ipAddress)
@@ -321,7 +293,6 @@ namespace WpfApp1
             }
             return addressBytes;
         }
-
 
         private void HandleClient(TcpClient localClient, TcpClient serverClient)
         {
@@ -359,7 +330,7 @@ namespace WpfApp1
                 logBox.ScrollToEnd(); // Auto scroll to end
             });
         }
-        //调试版本Log
+
         public void Log(string message, object variable = null)
         {
             Dispatcher.Invoke(() =>
@@ -374,14 +345,12 @@ namespace WpfApp1
             });
         }
 
-        
-
         static void SendEncryptedData(NetworkStream stream, byte[] encryptedData)
         {
             byte[] dataLength = BitConverter.GetBytes((short)encryptedData.Length);
-            Array.Reverse(dataLength);  // Always send length in big endian
+            Array.Reverse(dataLength); // Always send length in big endian
             stream.Write(dataLength, 0, dataLength.Length);
             stream.Write(encryptedData, 0, encryptedData.Length);
         }
-    }   
+    }
 }
